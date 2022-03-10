6df19163eae15a52b039183d387266493908c12c
James Protzman <jprotzman@zerofox.com>
2022-03-10 11:35:12 -0500

    add the user profile viewset and base permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers
-
+from profiles_api import serializers, models, permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -85,3 +84,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle deleting an object"""
         return Response({'method': 'DELETE', 'id': pk})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
